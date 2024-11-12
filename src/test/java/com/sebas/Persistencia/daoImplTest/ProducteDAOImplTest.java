f48722d1e3b953179dc261b6a3ceda461ee98321
SebastianARG <sebastianarengifog16@gmail.com>
2024-11-12 12:36:11 +0100

    Archivos test añadidos

---
+++ src/test/java/com/sebas/Persistencia/daoImplTest/ProducteDAOImplTest.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package com.sebas.Persistencia.daoImplTest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author sebas
+ */
+public class ProducteDAOImplTest {
+    
+    public ProducteDAOImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
